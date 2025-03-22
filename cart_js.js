b478df86b9a5dda6de2164a822cd2cb7f99fbf83
fishwhile <jxu2725@gmail.com>
2025-03-22 14:45:13 +0800

    Add files via upload
    加上優惠卷功能

---
+++ cart_js.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    // 偵測到"使用優惠卷"打勾
+    $('#coupon').change(function() {
+        if ($(this).is(':checked')) {
+            $('#couponInputWrapper').slideDown(); // 用滑動方式顯示
+        } else {
+            $('#couponInputWrapper').slideUp(); // 用滑動方式隱藏
+        }
+    });
+    // 優惠卷套用按鈕 折扣碼: S1254006、S1254051、S1122003會成功套用
+    $('#applyCoupon').click(function() {
+        var coupon=$('#couponCode').val();
+        if(coupon==''){
+            alert('請輸入優惠卷代碼');
+            return;
+        }else if(coupon=='S1254006' || coupon=='S1254051' || coupon=='S1122003'){
+            alert('優惠卷代碼成功套用');
+            return;
+        }else{
+            alert('優惠卷代碼錯誤');
+            $('#couponCode').val('');
+            return;
+        }
+    });
+    
+});
