202841c206626a69f918c2330543654f7d602739
fishwhile <jxu2725@gmail.com>
2025-05-10 22:24:51 +0800

    Add files via upload
    1. cart.html -> cart.php
    2. 若無登入，所有頁面點擊將導到login.php

--- index.php
+++ index.php
@@ -74,41 +74,21 @@ session_start();
                         <div class="logo pull-left">
                             <a href="index.php"><img src="images/home/logo.png" alt="" /></a>
                         </div>
-                        <div class="btn-group pull-right">
-                            <div class="btn-group">
-
-                                <button type="button" class="btn btn-default dropdown-toggle usa" data-toggle="dropdown">
-                                    <!-- language -->
-                                    語言
-
-                                    <span class="caret"></span>
-                                </button>
-                                <!-- <ul class="dropdown-menu">
-									<li><a href="">Chinese</a></li>
-									<li><a href="">USA</a></li>
-								</ul> -->
-                                <ul class="dropdown-menu">
-
-                                    <li><a href="">中文</a></li>
-
-                                </ul>
-                            </div>
-                        </div>
                     </div>
                     <div class="col-sm-8">
                         <div class="shop-menu pull-right">
                             <ul class="nav navbar-nav">
-                                <!-- <li><a href="#"><i class="fa fa-user"></i> Account</a></li> -->
-                                <li><a href="login.php"><i class="fa fa-user"></i> 帳號</a></li>
-                                <!-- <li><a href="checkout.html"><i class="fa fa-crosshairs"></i> Checkout</a></li> -->
-                                <li><a href="checkout.html"><i class="fa fa-crosshairs"></i> 查看歷史訂單</a></li>
-                                <!-- <li><a href="cart.html"><i class="fa fa-shopping-cart"></i> Cart</a></li> -->
-                                <li><a href="cart.html"><i class="fa fa-shopping-cart"></i> 購物車</a></li>
-                                <!-- <li><a href="login.html"><i class="fa fa-lock"></i> Login</a></li> -->
                                 <?php
-                                if(!isset($_SESSION['username'])) {
+                                if(isset($_SESSION['username'])) {
+                                    echo "<li><a href=\"checkout.html\"><i class=\"fa fa-crosshairs\"></i> 查看歷史訂單</a></li>";//若有登入導入到歷史訂單頁面
+                                    echo "<li><a href=\"cart.php\"><i class=\"fa fa-shopping-cart\"></i> 購物車</a></li>";//若有登入導入到購物車頁面
+                                    echo "<li><a href=\"profile.php\"><i class=\"fa fa-user\"></i> " . $_SESSION['username'] . "</a></li>";//顯示會員名稱 點下去即到個人資料頁面(未做)
+                                } else {//若沒有登入 不管點甚麼都導入到登入頁面
+                                    echo "<li><a href=\"login.php\"><i class=\"fa fa-user\"></i> 帳號 </a></li>";
+                                    echo "<li><a href=\"login.php\"><i class=\"fa fa-crosshairs\"></i> 查看歷史訂單</a></li>";
+                                    echo "<li><a href=\"login.php\"><i class=\"fa fa-shopping-cart\"></i> 購物車</a></li>";
                                     echo "<li><a href=\"login.php\"><i class=\"fa fa-lock\"></i> 登入</a></li>";
-                                } 
+                                }
                                 ?>
                             </ul>
                         </div>
@@ -132,20 +112,20 @@ session_start();
                         </div>
                         <div class="mainmenu pull-left">
                             <ul class="nav navbar-nav collapse navbar-collapse">
-
-                                <!-- <li><a href="index.html" class="active">Home</a></li> -->
                                 <li><a href="index.php" class="active">首頁</a></li>
-                                <!-- <li class="dropdown"><a href="#">Shop<i class="fa fa-angle-down"></i></a> -->
                                 <li class="dropdown"><a href="#">購物資訊<i class="fa fa-angle-down"></i></a>
                                     <ul role="menu" class="sub-menu">
-                                        <!-- <li><a href="shop.html">Products</a></li> -->
-                                        <li><a href="shop.php">商品</a></li>
-                                        <!-- <li><a href="checkout.html">Checkout</a></li> -->
-                                        <li><a href="checkout.html">歷史訂單</a></li>
-                                        <!-- <li><a href="cart.html">Cart</a></li> -->
-                                        <li><a href="cart.html">購物車</a></li>
-                                        <!-- <li><a href="login.html">Login</a></li> -->
-                                        <li><a href="login.php">登入</a></li>
+                                        <?php
+                                        if(isset($_SESSION['username'])) {//若有登入導入到對應頁面
+                                            echo "<li><a href=\"shop.php\">商品</a></li>";
+                                            echo "<li><a href=\"checkout.html\">歷史訂單</a></li>";
+                                            echo "<li><a href=\"cart.php\">購物車</a></li>";
+                                        } else {//若沒有登入 不管點甚麼都導入到登入頁面
+                                            echo "<li><a href=\"login.php\">商品</a></li>";
+                                            echo "<li><a href=\"login.php\">歷史訂單</a></li>";
+                                            echo "<li><a href=\"login.php\">購物車</a></li>";
+                                        }
+                                        ?>
 
                                     </ul>
                                 </li>
